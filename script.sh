72aaa4ed8b64c0342c4ea0f2d545414a2873ea0b
Mniac <zerocavs23@gmail.com>
2017-10-03 14:09:29 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -24,21 +24,6 @@ sudo apt-get update -y
 sudo apt-get install docker-ce -y
 sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-echo "web:
-  image: 'gitlab/gitlab-ce:latest'
-  restart: always
-  hostname: 'gitlab.example.com'
-  # environment:
-    # GITLAB_OMNIBUS_CONFIG: |
-      # external_url 'https://gitlab.example.com'
-      # Add any other gitlab.rb configuration here, each on its own line
-  ports:
-    - '80:80'
-    - '443:443'
-    - '26:22'
-  volumes:
-    - '/srv/gitlab/config:/etc/gitlab'
-    - '/srv/gitlab/logs:/var/log/gitlab'
-    - '/srv/gitlab/data:/var/opt/gitlab'" > docker-compose.yml
+curl https://raw.githubusercontent.com/Mniac/Gitlab/master/docker-compose.yml > docker-compose.yml
 docker-compose up -d
 
