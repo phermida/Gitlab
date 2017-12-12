95f3110651e4c2b5d8b0b89261454d61fb7fa328
phermida <zerocavs23@gmail.com>
2017-12-12 15:59:03 +0100

    Update Gitlab_install.sh

--- Gitlab/Gitlab_install.sh
+++ Gitlab/Gitlab_install.sh
@@ -3,7 +3,7 @@ apt-get update -y
 apt-get install docker.io -y
 apt-get install docker-compose -y
 initDir='/srv/gitlab'
-mv -r initconfig $initDir;
+cp -r initconfig $initDir;
 sed -i 's/Port 22/Port 2222/g' /etc/ssh/sshd_config
 service ssh restart
 echo "XXX.XXX.XXX.XXX:/gitlab /srv/gitlab nfs4" >> /etc/fstab
