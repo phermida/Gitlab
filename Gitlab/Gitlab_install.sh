f0d347be2cd574495d26840d8362369a5176455e
phermida <zerocavs23@gmail.com>
2017-12-13 13:23:56 +0100

    Update Gitlab_install.sh

--- Gitlab/Gitlab_install.sh
+++ Gitlab/Gitlab_install.sh
@@ -6,7 +6,7 @@ apt-get install docker.io -y
 apt-get install docker-compose -y
 cp -r initconfig /srv/gitlab/
 if [ ! -f "/srv/gitlab/data/.ssh/authorized_keys" ]; then mkdir -p /srv/gitlab/data/.ssh; touch /srv/gitlab/data/.ssh/authorized_keys; chown 998:998 /srv/gitlab/data/.ssh/authorized_keys; fi
-sed -i $'s/Port 22\\n/Port 2222\\n/g' /etc/ssh/sshd_config
+sed -i $'s/Port 22$/Port 2222$/g' /etc/ssh/sshd_config
 service ssh restart
 #echo "XXX.XXX.XXX.XXX:/gitlab /srv/gitlab nfs4" >> /etc/fstab
 docker-compose up -d
